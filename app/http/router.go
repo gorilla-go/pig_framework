cea76c1a8a47a5c10d3bfdaec2652347989c43f3
叶华 <799475742@qq.com>
2024-01-31 11:27:43 +0800

    update

--- app/http/router.go
+++ app/http/router.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Router(r *pig.Router) *pig.Router {
-	r.Static("/static/", config.RootPath()+"/log/access/2024-01/")
+	r.Static("/static/", config.RootPath()+"/static/")
 	r.GET("/", controller.Index).Name("index")
 	r.GET("/doc", func(context *pig.Context) {
 		context.Response().Redirect("https://pig.gitbook.io/p.i.g-framework")
